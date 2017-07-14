0116a934008b5ac3e65b5edb4536b6b9d4347258
Nazri Ramliy <ayiehere@gmail.com>
2017-07-14 08:44:28 +0800

    Remove: delete file only if it contains the right process id
    This avoids accidental removal of the pid file when a child process is
    forked from the parent, and writes its new process id to the same pid
    file.

--- pidrec.go
+++ pidrec.go
@@ -1,28 +1,63 @@
 package pidrec
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type PidFile struct {
 	path string
+	pid  int
 }
 
-// Remove deletes the file held by the PidFile.
-func (pf *PidFile) Remove() {
+// Remove deletes the file associated with the PidFile. The file is not deleted
+// if the process id recorded in the file is different than the one associated
+// with PidFile.
+func (pf *PidFile) Remove() error {
 	if pf == nil {
-		return
+		return nil
 	}
 	if len(pf.path) == 0 {
-		return
+		return nil
 	}
-	err := os.Remove(pf.path)
+	pid, err := getPid(pf.path)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	if pid != pf.pid {
+		return fmt.Errorf("%s: expecting %d, found %d", pf.path, pf.pid, pid)
+	}
+
+	return os.Remove(pf.path)
+}
+
+func getPid(file string) (int, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return -1, err
+	}
+	defer f.Close()
+	s := bufio.NewScanner(f)
+	i := 1
+	var str string
+	for s.Scan() {
+		if i > 1 {
+			return -1, fmt.Errorf("%s has spurious content", file)
+		}
+		str = s.Text()
+		i++
+	}
+	str = strings.Trim(str, " \t\n")
+	pid, err := strconv.Atoi(str)
+	if err != nil {
+		return -1, fmt.Errorf("%s: content not a number '%v'", file, err)
+	}
+	return pid, nil
 }
 
 // MustWriteTo writes the pid of the calling process to the given file.
@@ -40,9 +75,13 @@ func MustWriteTo(pidFile string) *PidFile {
 		log.Fatal("Pidfile already exist. Is another instance running?: ", pidFile)
 	}
 
-	err = ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d\n", os.Getpid())), 0644)
+	pid := os.Getpid()
+	err = ioutil.WriteFile(pidFile, []byte(fmt.Sprintf("%d\n", pid)), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &PidFile{pidFile}
+	return &PidFile{
+		path: pidFile,
+		pid:  pid,
+	}
 }
